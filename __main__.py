ae6b0cc9a79ec66bec445352cf363d5b3a907b36
Deadlocz <deadlocz26@gmail.com>
2024-02-27 13:43:27 +0100

    perlin noise got its own file, fixed some bugs, added Fractal Brownian Motion

--- __main__.py
+++ __main__.py
@@ -1,6 +1,6 @@
+import noise
 from rich.console import Console
-import math_helper as m
-import random
+
 
 """ default values """
 # x, y are "chunk coordinates"
@@ -9,21 +9,13 @@ y = 0
 # res is the resolution the chunk is getting generated in
 res = 16
 # amount of chunks in width and height
-chunksW = 3
+chunksW = 7
 chunksH = 3
 
 # console for color printing with rich.console
 console = Console()
+P, seed = noise.getPermutationT()
 
-
-class vect2:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def dot(self, other):
-        return self.x * other.x + self.y * other.y
-    
 def get_Color(value = 0):
     """ gets the correct color value, for a heightmap, with the given value """
     
@@ -87,87 +79,40 @@ def chunkDisplay(listData, resolution, width):
     
     return chunk    
     
-def getPermutationT(s=None):
-    """ generates a randomized permutation table for perlin noise """
-    P = []
-    [P.append(i) for i in range(256)]
-    
-    for i in range(len(P)-1,0,-1):
-        index = round(random.random()*(i-1))
-        temp = P[i]
-        
-        P[i] = P[index]
-        P[index] = temp
-    
-    P += P
-    return P
-
-seed = input("Seed (can be left empty): ")
-
-# generation of a random seed if none is given.
-if seed == "":
-    seed = random.randint(random.randint(0,65536),random.randint(65537,131072))
-else:
-    random.seed(seed)
-
-P = getPermutationT(seed)
-
-def perlin(x, y): 
-    """ generates perlin noise """
-       
-    def get_constant_vect(v):
-        h = v % 3
-        if h == 0:
-            return vect2( 1.0, 1.0)
-        elif h == 1:
-            return vect2(-1.0, 1.0)
-        elif h == 2:
-            return vect2(-1.0,-1.0)
-        else:
-            return vect2( 1.0,-1.0)
-
-    X = int(x) % 255
-    Y = int(y) % 255
-    
-    xf = x - int(x)
-    yf = y - int(y)
-    
-    topRight = vect2(xf-1.0, yf-1.0) 
-    topLeft  = vect2(xf,     yf-1.0)
-    botRight = vect2(xf-1.0, yf)
-    botLeft  = vect2(xf,     yf)
-    
-    vTopRight = P[P[X+1]+Y+1]
-    vTopLeft  = P[P[X  ]+Y+1]
-    vBotRight = P[P[X+1]+Y  ]
-    vBotLeft  = P[P[X  ]+Y  ]
-    
-    dTopRight = topRight.dot(get_constant_vect(vTopRight))
-    dTopLeft  = topLeft.dot (get_constant_vect(vTopLeft ))
-    dBotRight = botRight.dot(get_constant_vect(vBotRight))
-    dBotLeft  = botLeft.dot (get_constant_vect(vBotLeft ))
-    
-    u = m.fade(xf)
-    v = m.fade(yf)
-    
-    brightness = m.lerp2(u, m.lerp2(v, dBotLeft, dTopLeft), m.lerp2(v, dBotRight, dTopRight))
-    return brightness\
 
 def generateMap(x, y):
     """ generates a heighmap with given brightness data - here perlin noise """
+    
+    def FractalBrownianMotion(x, y, numOctaves):
+        """ Fractal Brownian Motion to create a more natural feel to the heightmap """
+        result = 0
+        amplitude = 1.0
+        frequency = 0.5
+        
+        for o in range(numOctaves):
+            n = amplitude * noise.perlin(x * frequency, y * frequency, P)
+            result += n
+
+            amplitude *= 0.5
+            frequency *= 2.0
+        
+        return result
+            
     data = []
     chunks = []
     
     xB = x
     
+
+    # freq res
     for i in range(res*chunksH):
         for j in range(res*chunksW):
-            data.append(perlin(x, y))
+            data.append(FractalBrownianMotion(x, y, 8))
             x += 1/res
-        
+                
         y += 1/res
         x = xB
-    
+
     chunks.append(chunkDisplay(data, res, chunksW))
     console.print(chunks[0])
     
@@ -211,12 +156,7 @@ def movement(x, y):
 
 """
 ! Minor Bugs:
-! - terrain becomes repetitive with too big of a "view"
-!  - easily visible at: (dunno if this is with EVERY seed tho)
-!    x = 0, y = 0
-!    res = 16
-!    chunksW = 6, chunksH = 5
-! - negative x and y values cause generation to basically 'die' lol
+! - sometimes generates values out of range of the colors
 """
 
 generateMap(x, y)
---
+++ noise.py
@@ -0,0 +1,77 @@
+import random
+import math_helper as m
+
+class vect2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y
+    
+
+def getPermutationT():
+    """ generates a randomized permutation table for perlin noise """
+    
+    s = input("Seed (can be left empty): ")
+    if s == "":
+        s = (random.randint(random.randint(0,65536),random.randint(65537,131072)))
+        random.seed(s)
+    else:
+        random.seed(s)
+        
+    P = []
+    [P.append(i) for i in range(256)]
+    
+    for i in range(len(P)-1,0,-1):
+        index = round(random.random()*(i-1))
+        temp = P[i]
+        
+        P[i] = P[index]
+        P[index] = temp
+    
+    P += P
+    return P,s
+
+
+def perlin(x, y, P): 
+    """ generates perlin noise for a single pixel using a given Permutation table `P` """
+       
+    def get_constant_vect(v):
+        h = v % 3
+        if h == 0:
+            return vect2( 1.0, 1.0)
+        elif h == 1:
+            return vect2(-1.0, 1.0)
+        elif h == 2:
+            return vect2(-1.0,-1.0)
+        else:
+            return vect2( 1.0,-1.0)
+
+    X = int(x) % 255
+    Y = int(y) % 255
+    
+    xf = x - int(x)
+    yf = y - int(y)
+    
+    topRight = vect2(xf-1.0, yf-1.0) 
+    topLeft  = vect2(xf,     yf-1.0)
+    botRight = vect2(xf-1.0, yf)
+    botLeft  = vect2(xf,     yf)
+    
+    vTopRight = P[P[X+1]+Y+1]
+    vTopLeft  = P[P[X  ]+Y+1]
+    vBotRight = P[P[X+1]+Y  ]
+    vBotLeft  = P[P[X  ]+Y  ]
+    
+    dTopRight = topRight.dot(get_constant_vect(vTopRight))
+    dTopLeft  = topLeft.dot (get_constant_vect(vTopLeft ))
+    dBotRight = botRight.dot(get_constant_vect(vBotRight))
+    dBotLeft  = botLeft.dot (get_constant_vect(vBotLeft ))
+    
+    u = m.fade(xf)
+    v = m.fade(yf)
+    
+    brightness = m.lerp2(u, m.lerp2(v, dBotLeft, dTopLeft), m.lerp2(v, dBotRight, dTopRight))
+    return brightness
+
